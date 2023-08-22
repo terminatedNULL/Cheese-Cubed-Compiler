79590ed6996e415e0c607b0ae19c9396e1e29e8d
terminatedNULL <alexander.yauchler@gmail.com>
2023-08-22 18:45:36 -0400

    Updated main for tests, added testing file

--- Source.cpp
+++ Source.cpp
@@ -2,10 +2,25 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "scan.c"
+
+#include "data.h"
+#include "definitions.h"
 
 int iine;
 int holdChar;
 FILE* inFile;
+
+char tokenStr[5][10] = {
+    "+",
+    "-",
+    "*",
+    "/",
+    "intLit"
+};
 
 int CALLBACK WinMain(
     HINSTANCE   hInstance,
@@ -16,5 +31,21 @@ int CALLBACK WinMain(
 {
     std::string debugFilePath = "testCheeseFile.txt";
 
+    inFile = fopen(debugFilePath.c_str(), "r");
+
+    scanFile();
+
 	return 0;
+}
+
+static void scanFile() {
+    token T;
+
+    while (scan(&T)) {
+        printf("Token %s", tokenStr[T.token]);
+        if (T.token == T_INT_LITERAL) {
+            printf(", value %d", T.intValue);
+        }
+        printf("\n");
+    }
 }
