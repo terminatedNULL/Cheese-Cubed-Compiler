1bebbc12e6cacf94e591cbe74ca5087e35f8c2b6
terminatedNULL <alexander.yauchler@gmail.com>
2023-08-22 01:51:21 -0400

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,16 @@
+#include <windows.h>
+#include <iostream>
+#include <string>
+#include <fstream>
+
+int CALLBACK WinMain(
+    HINSTANCE   hInstance,
+    HINSTANCE   hPrevInstance,
+    LPSTR       lpCmdLine,
+    int         nCmdShow
+)
+{
+    std::string debugFilePath = "testCheeseFile.txt";
+
+	return 0;
+}
